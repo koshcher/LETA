6c776bf0fabfa5a34b75e800ce0466f738e91bd7
koshcher <romankoshchey@gmail.com>
2021-10-27 13:42:09 +0300

    text

--- LCA-layouts/Main.cpp
+++ LCA-layouts/Main.cpp
@@ -2,7 +2,8 @@
 
 wxBEGIN_EVENT_TABLE(Main, wxFrame)
 
-	EVT_BUTTON(1, OnButtoClick)
+	EVT_BUTTON(1, OnButtonClick)
+	EVT_CHAR(OnChar)
 
 wxEND_EVENT_TABLE()
 
@@ -10,8 +11,8 @@ wxEND_EVENT_TABLE()
 Main::Main() : wxFrame(nullptr, wxID_ANY, "LCA layouts", wxPoint(30,30), wxSize(720, 480))
 {
 	m_btn = new wxButton(this, 1, "Start", wxPoint(10, 10), wxSize(150, 50));
-	m_text = new wxTextCtrl(this, wxID_ANY, "Start", wxPoint(10, 70), wxSize(300, 30));
-	m_list = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+	m_text = new wxStaticText(this, wxID_ANY, "Start", wxPoint(10, 70), wxSize(300, 40));
+	m_list = new wxListBox(this, wxID_ANY, wxPoint(10, 120), wxSize(300, 300));
 }
 
 Main::~Main() {
@@ -19,7 +20,24 @@ Main::~Main() {
 }
 
 
-void Main::OnButtoClick(wxCommandEvent& evt) {
-	m_list->AppendString(m_text->GetValue());
+void Main::OnButtonClick(wxCommandEvent& evt) {
+
+	m_list->AppendString(m_text->GetLabel());
+
+	evt.Skip();
+}
+
+void Main::OnChar(wxKeyEvent& evt) {
+	if (evt.GetKeyCode() == WXK_SPACE) {
+		m_list->AppendString(m_text->GetLabel());
+	}
+	else if (evt.GetKeyCode() == WXK_BACK) {
+
+		m_text->SetLabel(m_text->GetLabel().erase(m_text->GetLabel().Length() - 1));
+	}
+	else {
+		m_text->SetLabel(m_text->GetLabel() + char(evt.GetKeyCode()));
+	}
+
 	evt.Skip();
 }
--- LCA-layouts/Main.h
+++ LCA-layouts/Main.h
@@ -2,12 +2,13 @@
 #define MAIN_H
 
 #include "wx/wx.h"
+#include <wx/caret.h>
 
 class Main : public wxFrame
 {
 public: // компоненты
 	wxButton* m_btn = nullptr;
-	wxTextCtrl* m_text = nullptr;
+	wxStaticText* m_text = nullptr;
 	wxListBox* m_list = nullptr;
 
 	wxDECLARE_EVENT_TABLE();
@@ -16,8 +17,8 @@ public:
 	Main();
 	~Main();
 
-	void OnButtoClick(wxCommandEvent& evt);
-
+	void OnButtonClick(wxCommandEvent& evt);
+	void OnChar(wxKeyEvent& evt);
 };
 
 #endif
