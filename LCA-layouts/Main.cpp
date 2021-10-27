67320948da4a89593df2822fa742a2a2822858ce
koshcher <romankoshchey@gmail.com>
2021-10-27 11:57:42 +0300

    Add words file

--- LCA-layouts/Main.cpp
+++ LCA-layouts/Main.cpp
@@ -1,11 +1,25 @@
 #include "Main.h"
 
+wxBEGIN_EVENT_TABLE(Main, wxFrame)
 
-Main::Main() : wxFrame(nullptr, wxID_ANY, "LCA layouts")
+	EVT_BUTTON(1, OnButtoClick)
+
+wxEND_EVENT_TABLE()
+
+
+Main::Main() : wxFrame(nullptr, wxID_ANY, "LCA layouts", wxPoint(30,30), wxSize(720, 480))
 {
-
+	m_btn = new wxButton(this, 1, "Start", wxPoint(10, 10), wxSize(150, 50));
+	m_text = new wxTextCtrl(this, wxID_ANY, "Start", wxPoint(10, 70), wxSize(300, 30));
+	m_list = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
 }
 
 Main::~Main() {
 
+}
+
+
+void Main::OnButtoClick(wxCommandEvent& evt) {
+	m_list->AppendString(m_text->GetValue());
+	evt.Skip();
 }
--- LCA-layouts/Main.h
+++ LCA-layouts/Main.h
@@ -5,9 +5,18 @@
 
 class Main : public wxFrame
 {
+public: // компоненты
+	wxButton* m_btn = nullptr;
+	wxTextCtrl* m_text = nullptr;
+	wxListBox* m_list = nullptr;
+
+	wxDECLARE_EVENT_TABLE();
+
 public:
 	Main();
 	~Main();
+
+	void OnButtoClick(wxCommandEvent& evt);
 
 };
 
